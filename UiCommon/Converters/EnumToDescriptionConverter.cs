0659988c29074cbabef584c6844f489b6a7da844
luo <luodong@niuguwang.com>
2020-07-15 14:52:15 +0800

    增加界面的公用类

---
+++ UiCommon/Converters/EnumToDescriptionConverter.cs
@@ -0,0 +1,28 @@
+﻿using Common.Extensions;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace UiCommon.Converters
+{
+    public class EnumToDescriptionConverter : IValueConverter
+    {
+        private static EnumToDescriptionConverter _instance;
+
+        public static EnumToDescriptionConverter Instance
+        {
+            get { return _instance ?? (_instance = new EnumToDescriptionConverter()); }
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Enum @enum)) return value;
+            return @enum.GetDescription();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- UiCommon/Properties/AssemblyInfo.cs
+++ UiCommon/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Markup;
 
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
@@ -53,3 +54,8 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "UiCommon")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "UiCommon.Common")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "UiCommon.Converters")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "UiCommon.Extensions")]

b397623b39016af2df53eb8407ccf4b0ce5655a9
luo <luodong@niuguwang.com>
2020-07-22 11:26:22 +0800

    增加转换器

---
+++ UiCommon/Converters/IsTypeConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace UiCommon.Converters
+{
+    public class IsTypeConverter : IValueConverter
+    {
+        private static IsTypeConverter _instance;
+
+        public static IsTypeConverter Instance
+        {
+            get { return _instance ?? (_instance = new IsTypeConverter()); }
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter is Type type)
+            {
+                if (value == null)
+                {
+                    return !type.IsValueType;
+                }
+
+                if (type.IsValueType)
+                {
+                    return value.GetType() == type;
+                }
+                else
+                {
+                    return type.IsInstanceOfType(value);
+                }
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new InvalidOperationException($"{nameof(IsTypeConverter)}不支持TwoWay和OneWayToSource的绑定!");
+        }
+    }
+}
---
+++ UiCommon/Converters/SizeToRectConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace UiCommon.Converters
+{
+    public class SizeToRectConverter : IValueConverter
+    {
+        private static SizeToRectConverter _instance;
+
+        public static SizeToRectConverter Instance
+        {
+            get { return _instance ?? (_instance = new SizeToRectConverter()); }
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Size size)) return value;
+            return new Rect(size);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Rect rect)) return value;
+            return rect.Size;
+        }
+    }
+}

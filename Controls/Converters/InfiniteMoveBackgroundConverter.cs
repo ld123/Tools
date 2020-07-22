684aebcb8cf0febdd2f4a8f79050cde26ca97db5
luo <luodong@niuguwang.com>
2020-07-22 11:27:24 +0800

    增加控件项目

---
+++ Controls/Converters/InfiniteMoveBackgroundConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Controls.Converters
+{
+    public class InfiniteMoveBackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Border element)
+            {
+                if (element.Background is VisualBrush brush)
+                {
+                    return brush.Visual;
+                }
+                else
+                {
+                    return element;
+                }
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Controls/InfiniteMoveControl.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Controls
+{
+    /// <summary>
+    /// InfiniteMoveControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class InfiniteMoveControl : UserControl
+    {
+        public static readonly DependencyProperty PointProperty = DependencyProperty.Register(
+            "Point", typeof(Point), typeof(InfiniteMoveControl), new PropertyMetadata(default(Point), OnPointChanged));
+
+        private static void OnPointChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = (InfiniteMoveControl) d;
+            control.Point = (Point) e.NewValue;
+        }
+
+        public Point Point
+        {
+            get { return (Point) GetValue(PointProperty); }
+            set { SetValue(PointProperty, value); }
+        }
+
+        private Point? _lastMousePoint;
+        private Point _lastPoint;
+
+        public InfiniteMoveControl()
+        {
+            InitializeComponent();
+
+            MouseLeftButtonDown += OnMouseLeftButtonDown;
+            MouseMove += OnMouseMove;
+            MouseLeftButtonUp += OnMouseLeftButtonUp;
+            MouseLeave += OnMouseLeave;
+        }
+
+        private void OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            _lastMousePoint = null;
+        }
+
+        private void OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _lastMousePoint = null;
+            ReleaseMouseCapture();
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (_lastMousePoint == null) return;
+            var ptr = e.GetPosition(this) - _lastMousePoint.Value;
+            var x = ptr.X + _lastPoint.X;
+            var y = ptr.Y + _lastPoint.Y;
+
+            Point = new Point(x, y);
+        }
+
+        private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _lastMousePoint = e.GetPosition(this);
+            _lastPoint = Point;
+            CaptureMouse();
+        }
+    }
+}
